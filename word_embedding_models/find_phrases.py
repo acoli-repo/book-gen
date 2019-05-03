4694a33eeca485cc6f3110ee034d44edc15c5cd6
Samuel Rönnqvist <sronnqvist@users.noreply.github.com>
2019-05-03 17:07:35 +0300

    Update find_phrases.py

--- word_embedding_models/find_phrases.py
+++ word_embedding_models/find_phrases.py
@@ -1,7 +1,6 @@
 from gensim.models.phrases import Phrases, Phraser
 import nltk
 import sys
-#import collocations
 
 
 class Reader(object):
@@ -14,24 +13,19 @@ class Reader(object):
             for i, line in enumerate(open(file)):
                 line = line.strip()
                 yield nltk.tokenize.word_tokenize(line)
-                if not self.convert and i > 3000000:
+                if not self.convert and i > 3000000: # Limit number of lines at initialization
                     break
                 if i % 10000 == 0:
                     print("Counting",i)
 
 print("Reading from:", sys.argv[1:-1])
 print("Writing to:", sys.argv[-1])
-phrases = Phrases(Reader(sys.argv[1:-1]), min_count=50, threshold=0.5, scoring='npmi')
+phrases = Phrases(Reader(sys.argv[1:-1]), min_count=50, threshold=0.5, scoring='npmi') # Initialize bigram detector
 
 out = open(sys.argv[-1], "w")
-#word_freq = collocations.defaultdict(lambda: 0)
-for i, sent in enumerate(Reader(sys.argv[1:-1], convert=True)):
-    #for token in sent:
-    #    word_freq[token] += 1
-    #print(' '.join(phrases[sent]))
+# Scan files
+for i, sent in enumerate(Reader(sys.argv[1:-1], convert=True)): 
     out.write(' '.join(phrases[sent])+'\n')
     if i % 100000 == 0:
         print("Converting",i)
         print(' '.join(phrases[sent]))
-    #if i > 100:
-    #    break
